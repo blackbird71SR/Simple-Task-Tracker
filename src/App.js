bd025927b5f28d8d639991864747d491b872467e
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2022-12-27 21:11:24 -0600

    Add taks as global state and access that from Tasks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Hello-1',
+      day: 'Feb 8th at 2:45pm',
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: 'Hello-2',
+      day: 'Feb 8th at 2:45pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Hello-3',
+      day: 'Feb 8th at 2:45pm',
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
