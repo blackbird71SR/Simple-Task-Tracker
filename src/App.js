584f993cbc27dd9a62f1dd0fa6785f6d9bed85f7
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2022-12-29 22:56:48 -0600

    Add add task functionality and store it to state

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
     },
   ]);
 
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -48,7 +55,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
